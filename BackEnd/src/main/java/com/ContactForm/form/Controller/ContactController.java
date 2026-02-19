041fe18b5ee47b27714be4f9d8f6bfea48072cd2
Sourabh_g_ <saurabhbante11@gmail.com>
2026-02-19 21:31:56 +0530

    file

--- BackEnd/src/main/java/com/ContactForm/form/Controller/ContactController.java
+++ BackEnd/src/main/java/com/ContactForm/form/Controller/ContactController.java
@@ -1,3 +1,16 @@
+package com.ContactForm.form.Controller;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.CrossOrigin;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import com.ContactForm.form.Model.ContactRequest;
+import com.ContactForm.form.Service.EmailService;
+
 @RestController
 @RequestMapping("/api")
 @CrossOrigin(
